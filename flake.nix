d2c41d544829d962e8c68daead93a49da0988821
pdobbins23 <146784496+pdobbins23@users.noreply.github.com>
2025-10-23 23:19:32 -0700

    Initial flake

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "Web-slop email client thats actually good";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: { };
+}
